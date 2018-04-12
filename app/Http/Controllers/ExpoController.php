d4322a3f07f677edd0373a5db5e782d6437c0ca1
dhawalraut10 <dhawalraut@gmail.com>
2018-04-12 11:16:03 +0530

    empty company excepation handling

--- app/Http/Controllers/ExpoController.php
+++ app/Http/Controllers/ExpoController.php
@@ -541,7 +541,7 @@ class ExpoController extends Controller
                             }
                         }
                         else
-                            $expo['records']['expo'][$allData->localExpoId]['company'][] = [];
+                            $expo['records']['expo'][$allData->localExpoId]['company'] = array();
 
                     }
                     else
@@ -598,7 +598,7 @@ class ExpoController extends Controller
                             }
                         }
                         else
-                            $expo['records']['expo'][$allData->localExpoId]['company'][] = [];
+                            $expo['records']['expo'][$allData->localExpoId]['company'] = array();
                     }
                     $i++;
                 }
