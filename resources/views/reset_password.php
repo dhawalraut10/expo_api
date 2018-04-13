83d52512a98cc846ce22fb33097a355f3b89c545
dhawalraut10 <dhawalraut@gmail.com>
2018-04-13 15:01:29 +0530

    moving email view file to view folder

---
+++ resources/views/reset_password.php
@@ -0,0 +1,1 @@
+<p>Dear ".$data->customer_name.",</p><p> You have made a request to reset the password for Expo Services. <br/><br/> Your System Generated Password is : ".$data->password."</p><p>Kindly change your password after loggin in.</p><p>Thank you</p><p>Expo Services</p>
