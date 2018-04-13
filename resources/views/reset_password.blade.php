a67836f4e6f9b2a748bc676e193e3725b64778cc
dhawalraut10 <dhawalraut@gmail.com>
2018-04-13 16:17:04 +0530

    blade changes

--- resources/views/reset_password.blade.php
+++ resources/views/reset_password.blade.php
@@ -1,2 +1,2 @@
 
-<p>Dear <?php echo $data->customer_name;?>,</p><p> You have made a request to reset the password for Expo Services. <br/><br/> Your System Generated Password is : <?php echo $data->password;?></p><p>Kindly change your password after loggin in.</p><p>Thank you</p><p>Expo Services</p>
+<p>Dear <?php echo $customer_name;?>,</p><p> You have made a request to reset the password for Expo Services. <br/><br/> Your System Generated Password is : <?php echo $password;?></p><p>Kindly change your password after loggin in.</p><p>Thank you</p><p>Expo Services</p>
