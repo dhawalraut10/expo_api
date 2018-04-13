15c51c6ff8e808d062fd019794b5a06134f467fe
dhawalraut10 <dhawalraut@gmail.com>
2018-04-13 12:40:06 +0530

    email service included

--- app/Http/Controllers/ExpoController.php
+++ app/Http/Controllers/ExpoController.php
@@ -1224,14 +1224,4 @@ Thank you.";
         }
         return $this->returnResponse($response_array);
     }
-    protected function generateRandomString($length = 10) 
-    {
-        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-        $charactersLength = strlen($characters);
-        $randomString = '';
-        for ($i = 0; $i < $length; $i++) {
-            $randomString .= $characters[rand(0, $charactersLength - 1)];
-        }
-        return $randomString;
-    }
 }
